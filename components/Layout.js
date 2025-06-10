1e964c63f5da860a2a91fcaa88a45764170d4fff
prit1310 <pritsenjaliya1162@gmail.com>
2025-06-11 00:34:47 +0530

    fix : update rel icon of web

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children, title = "Prit Senjaliya - Portfolio" 
         <title>{title}</title>
         <meta name="description" content="Personal portfolio of Prit Senjaliya - Full Stack Developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
         <Navbar />
--- components/ThemeProvider.js
+++ components/ThemeProvider.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from "next-themes"
 
 export default function CustomThemeProvider({ children }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
     </ThemeProvider>
   )
